d9769448fdb65ed8ffc73a38da18e3be7eabe06d
samonar ikii <syahrulmunir96@gmail.com>
2024-11-20 10:49:04 +0700

    Time: 75 ms (99.92%), Space: 33.7 MB (82.87%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,30 +1,29 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        elements = set()  # Melacak elemen unik
+        elements = set()
         current_sum = 0
         max_sum = 0
         begin = 0
 
-        for end in range(len(nums)):
-            # Jika elemen duplikat ditemukan, geser begin hingga elemen unik
-            # print(f'end {nums[end]}:{end}, begin {nums[begin]}:{begin}, elemen {elements}, sum {current_sum}')
-            while nums[end] in elements:
-                elements.remove(nums[begin])
-                current_sum -= nums[begin]
-                begin += 1
-            
-            # Tambahkan elemen baru
-            elements.add(nums[end])
-            current_sum += nums[end]
+        for end in range(n): 
+            if nums[end] not in elements:
+                current_sum += nums[end]
+                elements.add(nums[end])
 
-            # Jika panjang jendela mencapai k, periksa max_sum
-            if end - begin + 1 == k:
-                max_sum = max(max_sum, current_sum)
-                # Geser jendela
-                elements.remove(nums[begin])
-                current_sum -= nums[begin]
+                if end - begin + 1 == k:
+                    if current_sum > max_sum:
+                        max_sum = current_sum
+                    
+                    current_sum -= nums[begin]
+                    elements.remove(nums[begin])
+                    begin += 1
+            else:
+                while nums[begin] != nums[end]:
+                    current_sum -= nums[begin]
+                    elements.remove(nums[begin])
+                    begin += 1
+                
                 begin += 1
-           
 
         return max_sum
