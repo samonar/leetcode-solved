6ace6c6e1381f631b1dade5897707121e66f8a20
samonar ikii <syahrulmunir96@gmail.com>
2024-11-22 14:44:04 +0700

    Time: 59 ms (71.3%), Space: 20.1 MB (20.6%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -3,11 +3,8 @@ class Solution:
         patterns = Counter()
 
         for row in matrix:
-            # balik=(val ^ row[0] for val in row)
-            # print(f'{tuple(val ^ row[0] for val in row)} - {row}')
             matrix_result=tuple(val ^ row[0] for val in row)
             patterns[matrix_result]+=1
-            # print(patterns)
         return max(patterns.values())
             
 
