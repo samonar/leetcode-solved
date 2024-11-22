342ebf7fce6b20c5fb2bbd6f8e469947f09cd3d9
samonar ikii <syahrulmunir96@gmail.com>
2024-11-22 14:50:45 +0700

    Time: 68 ms (51.85%), Space: 20.1 MB (20.6%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         patterns = Counter()
-
         for row in matrix:
             matrix_result=tuple(val ^ row[0] for val in row)
-            # matrix_result = tuple(row) if row[0] == 0 else tuple(bit ^ 1 for bit in row)
             patterns[matrix_result]+=1
         return max(patterns.values())
             
